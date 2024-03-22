9a99f9efb3cef2f1313aec02ba0b074763337a74
Muzammil Juda <150502425+Muzammil-Juda@users.noreply.github.com>
2024-03-22 02:10:52 +0000

    Changes Made for initialize method

--- tests/test_playlist.rb
+++ tests/test_playlist.rb
@@ -3,14 +3,12 @@ require './playlist'
 
 class TestPlaylist < Minitest::Test
   def test_experience_is_string
-    playlist = Playlist.new("Good", 5)
-    playlist.experience = "Great"
+    playlist = Playlist.new
     assert playlist.experience.is_a?(String), "Experience should be a string"
   end
 
   def test_rate_is_integer
-    playlist = Playlist.new("Good", 5)
-    playlist.rate = 5
+    playlist = Playlist.new
     assert playlist.rate.is_a?(Integer), "Rate should be an integer"
   end
 end
